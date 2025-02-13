d71bce1fd59d7946275e343b0329da53772ad9ec
Chirag Bitoni <chirag.bitoni@grapecity.com>
2025-02-13 23:40:58 +0530

    Added test cases

---
+++ src/main/java/com/demoqa/pages/elements/DynamicPropertiesPage.java
@@ -0,0 +1,9 @@
+package com.demoqa.pages.elements;
+
+import org.openqa.selenium.By;
+
+public class DynamicPropertiesPage extends ElementsPage {
+    public By enableAfterBtn = By.id("enableAfter");
+    public By colorChangeBtn = By.id("colorChange");
+    public By visibleAfter = By.id("visibleAfter");
+}
---
+++ src/test/java/com/demoqa/test/elements/DynamicPropertiesTest.java
@@ -0,0 +1,9 @@
+package com.demoqa.test.elements;
+
+import com.demoqa.base.BaseTest;
+
+public class DynamicPropertiesTest extends BaseTest {
+    /*
+        Test case ID: TC
+     */
+}
